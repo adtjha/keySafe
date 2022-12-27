59b894e25f8fba9aefa54b0f63e300ff234571c7
Aditya Jha <4075396+adtjha@users.noreply.github.com>
2022-12-27 13:19:03 +0530

    create theme

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,58 @@
+/// DashboardWrapper
 // bg-stone-500 text-white py-2 px-4 rounded-md border-2 border-transparent hover:border-stone-900
 
-export const Button = ({ clickFunc, classNames, innerText }) => {
-  return (
-    <button onClick={clickFunc} className={classNames}>
-      {innerText}
-    </button>
-  );
-};
+/// Navbar
+// group relative px-2 text-base font-bold font-mono flex flex-row items-center justify-evenly hover:underline hover:bg-primary-light hover:text-primary-text rounded-lg
+// px-2 rounded-lg text-base font-bold font-mono flex flex-row items-center justify-evenly hover:underline hover:bg-primary-light hover:text-primary-text
+
+/// api > details
+// col-span-2 w-fit bg-secondary-dark/80 hover:bg-secondary-dark  text-secondary-text font-mono text-sm hover:underline hover:font-bold hover:drop-shadow-lg px-4 py-2 rounded-md
+// col-span-2 w-fit bg-secondary-dark/80 hover:bg-secondary-dark  text-secondary-text font-mono text-sm hover:underline hover:font-bold hover:drop-shadow-lg px-4 py-2 rounded-md
+// col-span-2 w-fit bg-red-500/80 hover:bg-red-500 text-secondary-text font-mono text-sm hover:underline hover:font-bold hover:drop-shadow-lg px-4 py-2 rounded-md
+
+/// api > new
+// absolute left-0 hover:bg-primary-light-30 rounded-full p-2 w-min
+// col-span-2 w-fit bg-secondary-dark text-secondary-text font-mono font-bold hover:underline text-2xl px-6 py-2 rounded-md
+
+/// api > user > new
+// absolute left-0 hover:bg-primary-light-30 rounded-full p-2 w-min
+// w-fit bg-primary-light-30 hover:bg-primary-light rounded-md py-3 px-6 flex flex-row items-center justify-start space-x-4
+// col-span-2 w-fit bg-secondary-dark text-secondary-text font-mono font-bold hover:underline text-2xl px-6 py-2 rounded-md
+// w-full flex flex-row items-center justify-end
+// bg-secondary-dark/40 hover:bg-secondary-dark/60 text-background px-4 py-2 rounded-md font-bold font-sans
+// bg-secondary-light/40 hover:bg-secondary-light/60 text-primary-text px-4 py-2 rounded-md font-bold font-sans
+
+/// api > users
+// absolute -left-12 group hover:bg-primary-light-30 rounded-full p-2 w-min
+
+//// features
+// bg-primary-light text-primary-text font-bold mt-8 px-8 py-4 rounded-2xl text-lg hover:underline
+// bg-primary-light text-primary-text font-bold mt-8 px-8 py-4 rounded-2xl text-lg hover:underline
+// bg-primary-light text-primary-text font-bold mt-8 px-8 py-4 rounded-2xl text-lg hover:underline
+
+/// landing page
+// absolute top-8 right-8 bg-[#FAFDF7] px-8 py-3 rounded-2xl text-lg font-medium hover:underline
+// bg-primary-light mt-4 px-8 py-4 rounded-2xl text-lg font-medium hover:underline
+// bg-primary-light mt-4 px-8 py-4 rounded-2xl text-lg font-medium hover:underline
+
+/// profile
+// absolute left-0 hover:bg-primary-light-30 rounded-full p-2 w-min
+
+// background color types
+/**
+ * bg-stone-500
+ * bg-secondary-dark/80
+ * bg-red-500/80
+ * bg-primary-light
+ * bg-primary-light-30
+ * bg-secondary-dark
+ * bg-secondary-dark/40
+ * bg-[#FAFDF7]
+ */
+
+// position
+/**
+ * absolute
+ */
+
+//
